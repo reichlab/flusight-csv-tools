275507f8ad1ee9403de76f56a581c5f4c667b1cc
Abhinav Tushar <lepisma@fastmail.com>
2017-12-13 02:46:56 -0500

    Finish point inference

--- src/verify.ts
+++ src/verify.ts
@@ -1,16 +1,32 @@
 import Submission from './submission'
 import { targetIds, regionIds, headers } from './meta'
-import { TargetId, RegionId } from './interfaces'
+import { TargetId, RegionId, Bin } from './interfaces'
 import * as assert from 'assert'
 import { almostEqual } from 'almost-equal'
 import * as arrayEqual from 'array-equal'
+
+/**
+ * Tell whether the bins have uniform distribution
+ */
+function isUniform(bins: Bin[]): boolean {
+  // Skip the last bin which is sometimes different since that bin contains, e.g.
+  // all wili values from 13 to 100
+  return bins.slice(0, bins.length - 1).every(bin => almostEqual(bin[2], bins[0][2]))
+}
 
 /**
  * Infer point probability value for the submission using bins
  */
 function inferPoint(sub: Submission, target: TargetId, region: RegionId): number {
   let bins = sub.getBins(target, region)
-  return bins[0][2]
+
+  if (isUniform(bins)) {
+    return bins[Math.floor(bins.length / 2)][0]
+  } else {
+    let pointBin = bins.reduce((acc, item) => {
+      return (acc[2] < item[2]) ? item : acc
+    }, bins[0])
+  }
 }
 
 /**
