36d01ca70d415f46fe559f77d5123bd2dd10c0fe
Abhinav Tushar <lepisma@fastmail.com>
2017-12-25 18:05:08 -0500

    Make lag optional

--- src/delphi.ts
+++ src/delphi.ts
@@ -9,22 +9,28 @@ const API_ROOT = 'https://delphi.midas.cs.cmu.edu/epidata/api.php/'
 
 /**
  * Build url for API request. The url represents a request for all weeks
- * in a single epidemic season, all regions and 1 lag value.
+ * in a single epidemic season, all regions and single lag value.
+ * Lag can be skipped if needing latest issue (with max lag value)
  */
-function getSeasonRequestUrl(season: SeasonId, lag: number): string {
-  return buildUrl(API_ROOT, {
+function getSeasonRequestUrl(season: SeasonId, lag?: number): string {
+  let url = buildUrl(API_ROOT, {
     queryParams: {
       epiweeks: `${season * 100 + 30}-${(season + 1) * 100 + 29}`,
       source: 'fluview',
-      regions: regionIds,
-      lag: lag
+      regions: regionIds
     }
   })
+
+  return lag ? `${url}&lag=${lag}` : url
 }
 
 /**
  * Return a promise with data from delphi epidata API
+ * Note that the API only allows 3650 items for each request
+ * (https://github.com/cmu-delphi/delphi-epidata/issues/1#issuecomment-308502781)
+ * Collecting data for a season (with one lag value) with all regions amounts
+ * to max of 520 results
  */
-export function getSeasonData(season: SeasonId, lag: number): Promise<any> {
+export function getSeasonData(season: SeasonId, lag?: number): Promise<any> {
   return rp({ uri: getSeasonRequestUrl(season, lag), json: true })
 }
