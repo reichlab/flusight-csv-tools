55bf4c2e75471f706557b11348a3f0a56e98a623
Abhinav Tushar <lepisma@fastmail.com>
2017-12-25 17:41:38 -0500

    Use /promised/ writefile

--- src/truth.ts
+++ src/truth.ts
@@ -35,12 +35,12 @@ async function readCsv(fileName: string): Promise<Array<any>> {
 }
 
 /**
- * Download baseline csv file to given path
+ * Download baseline csv file to given path and return a promise for the path
  * @param outputFile
  */
-export async function downloadBaseline(outputFile: string): Promise<void> {
-  await download(BASELINE_URL).then(data => {
-    fs.writeFileSync(outputFile, data);
+export async function downloadBaseline(outputFile: string): Promise<string> {
+  return download(BASELINE_URL).then(data => {
+    fs.writeFile(outputFile, data).then(() => outputFile)
   })
 }
 
